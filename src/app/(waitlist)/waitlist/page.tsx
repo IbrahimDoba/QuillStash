4a0fad1eaf3d3a3be65e62bc5a29d766407d9f8a
Ibrahim doba <ibrahimdoba55@gmail.com>
2024-10-13 23:04:52 +0100

    created waitlist page

---
+++ src/app/(waitlist)/waitlist/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WaitlistComponent from './waitlistComponent'
+
+const Waitlist = () => {
+  return (
+    <div>
+      <WaitlistComponent/>
+    </div>
+  )
+}
+
+export default Waitlist
